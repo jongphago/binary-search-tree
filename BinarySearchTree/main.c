43977704db52a1994f1bd28778bab64701c7ab65
Jong Hyun Kim <jonghyun.d.kim@gmail.com>
2020-11-22 16:44:17 +0900

    BinarySearchTree를 연결리스트로 구현하였습니다. - 함수 [insert]를 호출하여 알맞은 위치에 인수로 받은 정수형 변수 'num'을 추가합니다. - '알맞은 위치'는 이진검색트리의 조건에 맞는 위치로 treeNode의 'data'의 값과 대소를 비교하여 값이 작으면 'lchild'에, 값이 크면 'rchild'에 treeNode의 포인터를 저장합니다. - '알맞은 위치'를 찾는 함수는 함수 [modifiedSearch]에 구현되어 있습니다. 'lchild'나 'rchild'로 가기전 포인터를 tempTree에 저장했다가 다음 tree가 NULL일 경우 tempTree를 반환 합니다.

--- BinarySearchTree/main.c
+++ BinarySearchTree/main.c
@@ -1,6 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX_TERMS 100
+#define IS_FULL(ptr) (!(ptr))
+
+typedef struct treeNode* treePtr;
+typedef struct treeNode {
+	treePtr lchild;
+	int data;
+	treePtr rchild;
+}treeNode;
+
+void inorder(treePtr);
+void preorder(treePtr);
+void insert(treePtr*, int);
+treePtr modifiedSearch(treePtr, int);
 
 void main(void)
+/* p207 program 5.1, 	p208 program 5.2, 	p235 program 5.17 insert 함수,
+		강의중 제시한 Program modifiedSearch 를 참고 */
 {
-	printf("Hello, World!");
+	int i, n, A[MAX_TERMS];
+	treePtr tree = NULL;
+
+	// 파일을 오픈한다.
+	FILE* inputFile = fopen("input.txt", "r");
+	// 파일을 읽어 정수의 개수는 n에, 삽입하여야할 정수들은 A에 저장한다.
+	fscanf(inputFile, "%d", &n);
+	for (int i = 0; i < n ; i++)
+		fscanf(inputFile, "%d", &(A[i]));
+
+	/* 순서대로 binary search tree에 삽입한다 */
+	for (i = 0; i < n; i++)
+		insert(&tree, A[i]);
+
+	/* 구성된 binary search tree를 inorder와 preorder로 출력하여 올바르게 트리가 
+		생성되었는지 확인한다. */
+	inorder(tree);
+	printf("\n");
+	preorder(tree);
+}
+
+void inorder(treePtr ptr)
+{
+	if (ptr) {
+		inorder(ptr->lchild);
+		printf("%3d", ptr->data);
+		inorder(ptr->rchild);
+	}
+}
+
+void preorder(treePtr ptr)
+{
+	if (ptr) {
+		printf("%3d", ptr->data);
+		inorder(ptr->lchild);
+		inorder(ptr->rchild);
+	}
+}
+
+void insert(treePtr* node, int num)
+{
+	/* if num is the tree pointed at by node do nothing;
+		otherwise add a new node with data = num*/
+	treePtr ptr, temp = modifiedSearch(*node, num);
+	if (temp || !(*node)) {
+		/* num is not in the tree */
+		ptr = (treePtr)malloc(sizeof(treeNode));
+		if (IS_FULL(ptr)) {
+			fprintf(stderr, "The memory is full");
+			exit(1);
+		}
+		ptr->data = num;
+		ptr->lchild = NULL; ptr->rchild = NULL;
+		if (*node)
+			if (num < temp->data)
+				temp->lchild = ptr;
+			else temp->rchild = ptr;
+		else *node = ptr;
+	}
+}
+
+treePtr modifiedSearch(treePtr tree, int key)
+{
+	treePtr tempTree = malloc(sizeof(treeNode));
+	if (!tree) 	return NULL;
+	while (tree)
+	{
+		if (key == tree->data) return NULL;
+		else if (key < tree->data) {
+			tempTree = tree;
+			tree = tree->lchild;
+		}
+		else {
+			tempTree = tree;
+			tree = tree->rchild;
+		}
+	}
+	return tempTree;
 }
